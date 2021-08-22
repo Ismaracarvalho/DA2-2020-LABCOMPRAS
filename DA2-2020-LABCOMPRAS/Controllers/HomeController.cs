d27a9b3a2e8cdc394c8d20bd25310d9ccb3ea438
Ismara Valkiria Andrade Carvalho <160323054@estudantes.ips.pt>
2021-08-22 01:38:42 +0100

    controller e view

--- DA2-2020-LABCOMPRAS/Controllers/HomeController.cs
+++ DA2-2020-LABCOMPRAS/Controllers/HomeController.cs
@@ -6,21 +6,10 @@ using System.Threading.Tasks;
 
 namespace DA2_2020_LABCOMPRAS.Controllers
 {
-    //Passo 2.
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            ViewData["Title"] = "Entrada";
-
-            return View();
-        }
-
-        //Passo 5.
-        public IActionResult Conceito()
-        {
-            ViewData["Title"] = "Conceito";
-
             return View();
         }
     }
